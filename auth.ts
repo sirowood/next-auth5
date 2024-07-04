662e562aedff71c73c0aa611c07a516314045db7
Xuefeng Wu <sirowood@gmail.com>
2024-07-04 11:22:26 +0300

    feat: add user role to database and session

--- auth.ts
+++ auth.ts
@@ -1,8 +1,25 @@
-import NextAuth from 'next-auth';
+import NextAuth, { DefaultSession } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 
 import { db } from '@/lib/db';
 import authConfig from '@/auth.config';
+import { getUserById } from './data/user';
+import { UserRole } from '@prisma/client';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      role?: UserRole;
+    } & DefaultSession['user'];
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role?: UserRole;
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +27,34 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async jwt({ token }) {
+      if (!token || !token.sub) {
+        return token;
+      }
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) {
+        return token;
+      }
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
